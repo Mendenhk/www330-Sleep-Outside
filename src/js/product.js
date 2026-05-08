420b83906c975f80764b8a73908d104b361d5e76
Mendenhk <kris_mendenhall@yahoo.com>
2026-05-08 15:00:14 -0700

    wk1 ind.act. Step 4: code was changed according to the given solution.  Primary changes: 1. removal of code to convert to and from JSON. 2. replace above code with functions which do JSON conversion (getLocalStorage() and setLocalStorage()) 3. getLocalStorage had to be imported as it had not been done so (setLocalStorage had been imported by the assignment creator).

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  //km wk1 ind. act. step 3-1 (next 3 lines of code).  Five steps: retrieve data, convert to object, add item, convert back to JSON, save item.
-  let cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
-  cartItems.push(product);
-  localStorage.setItem("so-cart", JSON.stringify(cartItems));
+  //km wk1 ind. act. step 3-1 (next 3 lines of code).  Five steps: retrieve data, convert to object, add item, convert back to JSON, save item.  (the actual solution, see below, did not include JSON conversion for the reasons stated below.  As such my code can be deleted-below three lines of code)
+  // let cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
+  // cartItems.push(product);
+  // localStorage.setItem("so-cart", JSON.stringify(cartItems));
   // setLocalStorage("so-cart", product);  //original code replaced by above three lines (to be deleted)
+  //below, the actual solution.  Note that JSON conversion is missing.  These are handled by the function setLocalStorage().  
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
