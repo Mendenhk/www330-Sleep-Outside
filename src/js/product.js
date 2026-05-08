5f50c7b221a726381798a05e1ab0200e409e6af9
Mendenhk <kris_mendenhall@yahoo.com>
2026-05-07 23:21:43 -0700

    wk1 ind. activity. Step 3-1 and Step 3-1.  added code to save more than one item into localstorage rather than overwriting existing.  (need to view example solution)

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,11 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //km wk1 ind. act. step 3-1 (next 3 lines of code).  Five steps: retrieve data, convert to object, add item, convert back to JSON, save item.
+  let cartItems = JSON.parse(localStorage.getItem("so-cart")) || [];
+  cartItems.push(product);
+  localStorage.setItem("so-cart", JSON.stringify(cartItems));
+  // setLocalStorage("so-cart", product);  //original code replaced by above three lines (to be deleted)
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
