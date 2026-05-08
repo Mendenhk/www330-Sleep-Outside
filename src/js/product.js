8cb06cb12ce60c65b60425f827c7e54414f0a875
schoolonnie <lsmith19@byupathway.edu>
2026-05-08 16:51:22 -0500

    Individual assignment 1

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+// --bellow was my original solution, but I made it more like the example solution by moving it into the function because it seems less likely to encounter bugs by being a temporary variable.
+//let cartList = getLocalStorage("so-cart") || [];
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartList = getLocalStorage("so-cart") || [];
+  cartList.push(product);
+  setLocalStorage("so-cart", cartList);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
