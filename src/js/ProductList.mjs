f4f5ba12bb12e8a6c8990a43f35b286a2a731365
Zaanian <sstauffer1@byupathway.edu>
2026-05-15 11:14:57 -0700

    following individual instructions, step 5

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,14 @@
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/?product=">
+      <img src="" alt="Image of ">
+      <h2 class="card__brand"></h2>
+      <h3 class="card__name"></h3>
+      <p class="product-card__price">$</p>
+    </a>
+  </li>`
+}
+
 //following class extracts JSON files
 export default class ProductList {
   constructor(category, dataSource, listElement) {
@@ -9,3 +20,8 @@ export default class ProductList {
     const list = await fetch(this.dataSource);
   }
 }
+
+function renderlist(list) {
+  const htmlStrings = list.map(productCardTemplate)
+  this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''))
+};
