ce014edb8a74fedd53d34786178a22f408e5aaaf
Mendenhk <kris_mendenhall@yahoo.com>
2026-05-18 16:49:28 -0700

    changed ..public/json/${this. Category}.json to ../json/${this. Category}.json` in ProductData.mjs

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -10,7 +10,7 @@ export default class ProductData {
   constructor(category) {
     this.category = category;
     // this.path = `../json/${this.category}.json`;
-    this.path = `../public/json/${this.category}.json`;
+    this.path = `../json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
